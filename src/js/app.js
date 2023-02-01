87664725a6e2251ff9c2b93e32085431fcb66268
idgidikov <idgidikov@abv.bg>
2023-02-01 22:35:47 +0200

    fix : app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,25 @@
-import "../scss/app.scss";
+//  import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const main = document.querySelector(".main")
+  const image = document.createElement("div")
+  image.className ="image";
 
+  const img = document.createElement("img");
+  img.src = "https://images.pexels.com/photos/1918246/pexels-photo-1918246.jpeg"
+  image.appendChild(img);
+  
+  main.appendChild(image);
+
+  const btn = document.querySelector(".image")
+  btn.addEventListener("click", doubleImage);
+
+  function doubleImage() {
+    image.style.transform = "scale(2)";
+
+
+  }
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
